ae0b24a61097423a119c1d7d405f293b0cc808d9
Victor Izquierdo <vhmizq@gmail.com>
2021-11-21 14:39:38 -0600

    challenges about desestructuring objects and templeate literals

---
+++ DesesttructuringObjects.js
@@ -0,0 +1,32 @@
+//Desestructuring objects
+const player = {
+  fullName: "Michael Jordan",
+  club: "LA Lakers",
+  address:{
+    city: "Los Angeles"
+  }
+};
+console.log(player.fullName);
+console.log(player.address.city);
+
+const {fullName, club, address: {city}} = player;
+//console.log(`${fullName} plays for ${club}`)
+console.log(`${fullName} lives in ${city}`);
+
+/*
+    **** Challenge ****
+    
+    For this challenge destruture the following object.
+*/
+
+const student = {
+  name: "Kyle",
+  age: 24,
+  projects: {
+      diceGame: "Two player dice game using JavaScript"
+  }
+};
+
+const {name, age, projects: {diceGame}} = student;
+
+console.log(`${name} is ${age} years old and has a project game about ${diceGame}`);
---
+++ TemplateLiterals.js
@@ -0,0 +1,9 @@
+//Template literals
+/* let firstName = "Victor";
+let lastName = "Izquierdo";
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName); */
+
+let greetings = `Hello World! \n` + `from Jupiter`;
+console.log(greetings); 
+
